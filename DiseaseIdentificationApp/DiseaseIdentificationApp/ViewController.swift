373be9a4ce2d5782b10f237341241f679295883a
AFFLatusss <jasonyee1999@gmail.com>
2023-07-16 15:49:23 +0100

    created swift base

---
+++ DiseaseIdentificationApp/DiseaseIdentificationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiseaseIdentificationApp
+//
+//  Created by 余荣键 on 16/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
