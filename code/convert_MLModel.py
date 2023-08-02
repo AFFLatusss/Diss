b5042904f024441fefbabfccb93292eaa9f52fa0
AFFLatusss <jasonyee1999@gmail.com>
2023-08-02 11:24:43 +0100

    convert Model

---
+++ code/convert_MLModel.py
@@ -0,0 +1,27 @@
+import torch
+import coremltools as ct
+from torchvision import transforms
+
+
+
+def convert_to_mlmodel(model, class_labels, name, test_img):
+    scale = 1/(0.226*255.0)
+    bias = [- 0.485/(0.229) , - 0.456/(0.224), - 0.406/(0.225)]
+
+    image_input = ct.ImageType(name="x_1",
+                            shape=test_img.shape,
+                            scale=scale, bias=bias)
+    
+    traced_model = torch.jit.trace(model, test_img)
+    # out = traced_model(img1)
+    # print(out)
+
+    ios_model = ct.convert(
+        traced_model,
+        inputs=[image_input],
+        classifier_config = ct.ClassifierConfig(class_labels),
+        # compute_units=ct.ComputeUnit.CPU_ONLY,
+    )
+
+    ios_model.save("ML_Models/"+ name +".mlmodel")
+    
